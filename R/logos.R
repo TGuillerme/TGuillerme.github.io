6827adef55fc1aadc075c1e9e168b1e9d75c3658
Thomas Guillerme <guillert@tcd.ie>
2017-11-03 14:51:46 +1000

    Updated research page

---
+++ R/logos.R
@@ -0,0 +1,15 @@
+## Colors
+white <- "#D9DFC0"
+grey_sharp <- "#1C1C1C"
+grey_light <- "#4A4A4A"
+orange_sharp <- "#F65205"
+orange_light <- "#F38336"
+blue_sharp <- "#3E9CBA"
+blue_light <- "#98D4CF"
+
+## Path
+PATH <- "../images/"
+
+## Size
+heigth <- 180
+width <- 180
